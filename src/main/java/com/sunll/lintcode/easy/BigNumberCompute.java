275b547bac3e6503e3c394f9812db635eeffc115
sunliangliang <397585361@qq.com>
2019-08-21 20:58:18 +0800

    init

--- src/main/java/com/sunll/lintcode/easy/BigNumberCompute.java
+++ src/main/java/com/sunll/lintcode/easy/BigNumberCompute.java
@@ -1,5 +1,7 @@
 package com.sunll.lintcode.easy;
 
+import java.util.Map;
+
 /**
  * <p>desc: 大数计算</p>
  * 这里的大数指的是用长整型已经容不下了，当然了类似Biginterger这种大数类就不考虑了
@@ -12,8 +14,117 @@ package com.sunll.lintcode.easy;
  */
 public class BigNumberCompute {
     public static void main(String[] args) {
+        String a = "123";
+        String b = "3959";
+        System.out.println(bignumAdd(a,b));
+        System.out.println(bignumSubtract(a,b));
+        System.out.println(bignummultiply(a,b));
 
     }
 
+    /**
+     * 大数加法
+     * 思想：先把位数补齐，从个位开始计算，用一个临时变量记录是否需要进一位
+     * @param a
+     * @param b
+     * @return
+     */
+    public static String bignumAdd(String a, String b){
+        if (a.length() < b.length()){
+            String tmp = a;
+            a = b;
+            b = tmp;
+        }
+        int cha = a.length() - b.length();
+        for (int i = 1; i <= cha;i++){
+            b = '0' + b;
+        }
+        StringBuilder res = new StringBuilder();
+        int flag = 0;
+        for (int i = a.length()-1; i>=0; i--){
+            int sum = a.charAt(i) - 48 + b.charAt(i) - 48 + flag;
+            flag = sum/10;
+            res.append(sum % 10);
+        }
+        return res.reverse().toString();
+    }
 
+    public static String bignumSubtract(String a, String b){
+        StringBuilder res = new StringBuilder();
+        String fuhao = "";
+        if (a.length() < b.length() || (a.length() == b.length() && a.compareTo(b) < 0)){//如果b>a，那么就交换并且符号为负号
+            fuhao = "-";
+            String tmp = a;
+            a = b;
+            b = tmp;
+        }
+        int cha = a.length() - b.length();//补0
+        for (int i = 1; i <= cha;i++){
+            b = '0' + b;
+        }
+        //同样的套路，只不过现在不是进位，而是借位了
+        int flag = 0;
+        for (int i = a.length() - 1; i >= 0; i--){
+            int subtract = a.charAt(i) - b.charAt(i) + flag;
+            if (subtract < 0){
+                flag = -1;
+                subtract = Math.abs(subtract);//负数变正数
+            }else{
+                flag = 0;
+            }
+            res.append(subtract);
+        }
+        res.append(fuhao);
+        return res.reverse().toString();
+    }
+
+    /**
+     * 大数乘法
+     * 跟加减法不一样的是不是对位运算进位或者借位了，而是将一个数拆开然后计算，比如123*45=123*45 + 123*5
+     * @param a
+     * @param b
+     * @return
+     */
+    public static String bignummultiply(String a, String b){
+        String res = "";
+        for (int i = 0; i < b.length(); i++){
+            String tmp = compute(a,b.charAt(i));//每次用一位去乘
+            tmp = add0(tmp,b.length() - i - 1);//比如123*40，用4乘完之后要补一个0
+            res = bignumAdd(res, tmp);
+        }
+        return res;
+    }
+    private static String compute(String a, char b){
+        int realNum = b - '0';
+        int flag = 0;
+        StringBuilder res = new StringBuilder();
+        for (int i = a.length() - 1; i >= 0; i--){
+            int ji = (a.charAt(i) - '0') * realNum + flag;
+            flag = ji / 10;
+            res.append(ji % 10);
+        }
+        return res.reverse().toString();
+    }
+    private static String add0(String a, int total){
+        for (int i = 0; i < total; i++){
+            a += "0";
+        }
+        return a;
+    }
+
+    /**
+     * 大数除法
+     * 思想：跟加减乘法完全不一样了！！
+     * 比如128 / 3，可以看作128 / 30 = 4 余8，此轮结果是4*10=40
+     * 下一轮 8 / 3，只能看作是8 / 3  = 2 余2，此轮结果是2*1
+     * 综上结果=40 + 2
+     * 关键点在于怎么计算128 / 30 或者8 / 3，思想就是用大数减法，比如123 减四次30 就余8了
+     * @param a
+     * @param b
+     * @return
+     */
+    public static String bignumDivide(String a, String b){
+
+        return null;
+    }
 }
--- src/main/java/com/sunll/lintcode/normal/LS.java
+++ src/main/java/com/sunll/lintcode/normal/LS.java
@@ -1,10 +1,13 @@
 package com.sunll.lintcode.normal;
 
+
+import java.util.*;
+
 /**
  * <p>desc: 最长无重复字串</p>
  * 输入是abcdabc，输出是4或者abcd
  * 暴力方法：O(n2),都可以想到的
- * 动态规划方法：借助空间O(n)的链表或者
+ * 动态规划方法：借助空间O(n)的链表或者set或者hashmap或者数组
  * 动态规划方法：用指针记录，不太容易理解
  * @author sunliangliang 2019-08-21 09:08
  * @version 1.0
@@ -13,23 +16,67 @@ public class LS {
     public static void main(String[] args) {
         String str = "abcdabc";
         System.out.println(ls01(str));
+        System.out.println(ls02(str));
+        System.out.println(ls03(str));
     }
 
     /**
-     * 动态规划：借助O(n)空间来实现
-     * 思想：
+     * 第一种方案
+     * 动态规划：借助O(256)空间来实现
+     * 思想：使用int[256]，借助额外空间
+     * 每次读取一个字符，先去int[256]中找，如果没有的话left指针不变；如果int[256]中存在了，那么left指针进行移动最后不管int[256]中存在与否，既然读取到了就要加到int[256]中
+     * 整个过程中结果是动态规划的。
      * @param str
      * @return
      */
     public static String ls01(String str){
-
-
-        return null;
+        int[] chars = new int[256];//ascii码表最多表示256个字符（扩展ascii表是256，一般的是128个）
+        int left = 0, res = 0;
+        for (int i = 0; i<str.length(); i++){
+            left = left < chars[str.charAt(i)]? chars[str.charAt(i)]:left;//一定要理解这一步和第三步，目的是把left移动到上一次出现该字符的下一个位置。
+            res = res < i-left+1? i-left+1:res;
+            chars[str.charAt(i)] = i+1;
+        }
+        return str.substring(left, left + res);//注意这里返回的是最后的最长的，如果返回第一个最长的话就再加一个指针即可。不必多说
     }
 
-    public static String ls02(){
-
-        return null;
+    /**
+     * 第二种方案：借助O(n)的set空间，时间复杂度是O(2n)的其实和O(n)一样了
+     * 思想：如果zaiset没有窗口就增大，同时结果动态规划变大。如果set中不存在，这里是关键，会移除left指针的元素，然后再次进set判断，是不是删除了重复的元素，如果还存在
+     * 说明刚才删的元素还没有到删除的元素那里，所以继续删除，直到删除了重复的元素，这样就可以保证窗口哪元素不重复了！
+     * @param str
+     * @return
+     */
+    public static String ls02(String str){
+        Set<Character> set = new HashSet<Character>();
+        int left = 0, res = 0;
+        for (int i = 0; i<str.length(); i++){
+            if (!set.contains(str.charAt(i))){
+                set.add(str.charAt(i));
+                res = Math.max(res, set.size());
+            }else{
+                set.remove(str.charAt(left++));
+            }
+        }
+        return str.substring(left, res+left);
     }
 
+    /**
+     * 第三种方案同样是借助O(n)的空间复杂度，时间复杂度是O(n)
+     * 思想：使用hashmap，key是字符，value是出现的索引，跟使用int[256]的思想基本一致
+     * @param str
+     * @return
+     */
+    public static String ls03(String str){
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, res = 0;
+        for (int i = 0; i < str.length(); i++){
+            if (map.containsKey(str.charAt(i))){
+                left = Math.max(left, map.get(str.charAt(i)));
+            }
+            res = Math.max(res, i-left+1);
+            map.put(str.charAt(i), i+1);
+        }
+        return str.substring(left, left + res);
+    }
 }
