90c638c445ac996ceac1d240df52b017223ff2c2
sunliangliang <397585361@qq.com>
2019-09-05 23:53:52 +0800

    init

--- src/main/java/com/sunll/lintcode/easy/ReverseLink.java
+++ src/main/java/com/sunll/lintcode/easy/ReverseLink.java
@@ -14,19 +14,23 @@ public class ReverseLink {
     public static void main(String[] args) {
         MyNode node4 = new MyNode(4, null);
         MyNode node3 = new MyNode(3, node4);
-        MyNode node2 = new MyNode(2, node3);
-        MyNode node1 = new MyNode(1, node2);
+        MyNode node2 = new MyNode(3, node3);
+        MyNode node1 = new MyNode(4, node2);
         MyNode head = node1;
-        while(null != head){
-            System.out.println(head.value);
-            head = head.next;
-        }
-        System.out.println("-------------");
-        MyNode head2 = reverseLink(node1);
-        while(null != head2){
-            System.out.println(head2.value);
-            head2 = head2.next;
-        }
+//        while(null != head){
+//            System.out.println(head.value);
+//            head = head.next;
+//        }
+//        System.out.println("-------------");
+//        MyNode head2 = reverseLink(node1);
+//        while(null != head2){
+//            System.out.println(head2.value);
+//            head2 = head2.next;
+//        }
+
+        //System.out.println(isPalindromic(head));
+        node4.next = node1;
+        System.out.println(isCycle(head));
     }
 
     /**
@@ -49,17 +53,61 @@ public class ReverseLink {
 
     /**
      * 判断是否是回文的单向链表
+     * 思路：该题考察的还是单向链表反转，但是一开始我只想到了空间复杂度为0(n)的，很简单就是进行反转然后依次比较即可，但是要求是时间和空间复杂度都是O（n）
+     * 后来经过面试官提示：并不需要把链表全部反转！！！我一想，我去，只需要反转一般而且还不用占用额外空间
      * @param head
      * @return
      */
     public static boolean isPalindromic(MyNode head){
-
-        return false;
+        int count = 0;
+        MyNode cur = head;
+        while (cur != null){
+            count++;
+            cur = cur.next;
+        }
+        if (count <= 1) return false;
+        int mid = count / 2;
+        //开始反转mid个节点
+        MyNode pre = null;
+        MyNode tmp = head;
+        while (mid > 0){
+            tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+            mid--;
+        }
+        if (count % 2 != 0) head = head.next;//如果是奇数个
+        while (pre != null && head != null){
+            if (pre.value == head.value){
+                pre = pre.next;
+                head = head.next;
+            }else {
+                return false;
+            }
+        }
+        return true;
     }
 
+    /**
+     * 判断链表是否是有环的
+     * 思想：用两个指针往后跑，一个指针是另一个指针速度的二倍，那么如果存在环的话，肯定会在某时刻相遇
+     * 如果存在环，那么相遇点就是环的入口，从此时开始计数，再碰见一次，慢的计数就是环的长度
+     * @param head
+     * @return
+     */
     public static boolean isCycle(MyNode head){
-
-        return false;
+        if (null == head || null == head.next) return false;
+        MyNode slow = head.next;
+        MyNode fast = head.next.next;
+        while (slow != fast){
+            if (null == fast || null == fast.next){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 
 
--- src/main/java/com/sunll/lintcode/hard/BSTreeUtils.java
+++ src/main/java/com/sunll/lintcode/hard/BSTreeUtils.java
@@ -1,12 +1,10 @@
 package com.sunll.lintcode.hard;
 
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.*;
 
 /**
  * 二叉查找树的一系列问题
- * 1.二叉查找树的增删改查？
+ * 1.二叉查找树的增删查？
  * 2.二叉树遍历（先中后层序遍历）？（普通二叉树）
  * 3.判断二叉树是否为对称的？（普通二叉树）(小米)
  * 4.二叉树的序列与反序列化方法？（普通二叉树）（快手）
@@ -18,7 +16,7 @@ public class BSTreeUtils<T extends Comparable<? super T>> {
 
     //-------------------
     //定义二叉树节点类
-    class Node<T extends Comparable<? super T>>{
+    static class Node<T extends Comparable<? super T>>{
         T data;
         Node leftChild, rightChild;//左右孩子
         public Node(T data){
@@ -30,12 +28,12 @@ public class BSTreeUtils<T extends Comparable<? super T>> {
     }
     //------------------
 
-    private Node root;
+    private Node root = null;
 
     /**
-     * 1. BSTree的查找
+     * 1.1 BSTree的查找
      */
-    private Node find(T key){
+    public Node find(T key){
         return find(key, root);
     }
     private Node find(T key, Node cur){
@@ -47,25 +45,218 @@ public class BSTreeUtils<T extends Comparable<? super T>> {
     }
 
     /**
-     * 2. BSTree的新增节点
+     * 1.2 BSTree的新增节点
      * - 若二叉排序树为空，则待插入结点*S作为根结点插入到空树中；
      * - 当非空时，将待插结点关键字S->key和树根关键字t->key进行比较，若s->key = t->key,则无须插入，若s->key< t->key,则插入到根的左子树中，若s->key> t->key,则插入到根的右子树中。而子树中的插入过程和在树中的插入过程相同，如此进行下去，直到把结点*s作为一个新的树叶插入到二叉排序树中，或者直到发现树已有相同关键字的结点为止。
      */
-    private void insert(T key){
-        insert(key, root);
+    public void insert(T key){
+        root = insert(key, root);
     }
-    private void insert(T key, Node cur){
-        if (cur == null) cur = new Node(key);
+    private Node insert(T key, Node cur){
+        if (cur == null) {
+            return new Node(key);
+        }
         int cmp = cur.data.compareTo(key);
-        if (cmp > 0) insert(key, cur.leftChild);
-        if (cmp < 0) insert(key, cur.rightChild);
+        if (cmp > 0) cur.leftChild = insert(key, cur.leftChild);
+        if (cmp < 0) cur.rightChild = insert(key, cur.rightChild);
+        return cur;///这一步太TM关键了，把根指针指回了root
     }
+
+    /**
+     * 1.3 BSTree的删除节点
+     * 思想：比较复杂，如果删除的节点的左右子树都不为空怎么办？那就把右子树的最小节点放在cur的位置，左子树还是左子树，右子树还是右子树，不过要把那个右子树的最小节点删除，因为已经移到cur了嘛
+     */
+    public void delete(T key){
+        root = delete(key, root);
+    }
+    private Node delete(T key, Node cur){
+        if (cur == null) return null;
+        int cmp = cur.data.compareTo(key);
+        if (cmp > 0) cur.leftChild = delete(key, cur.leftChild);
+        if (cmp < 0) cur.rightChild = delete(key, cur.rightChild);
+        if (cmp == 0) {
+            if (cur.leftChild == null) return cur.rightChild;
+            if (cur.rightChild == null) return cur.leftChild;
+            Node tmp = cur;
+            cur = getMin(cur.rightChild);
+            cur.leftChild = tmp.leftChild;
+            cur.rightChild = deleteMin(tmp.rightChild);
+        }
+        return cur;
+    }
+    private Node getMin(Node node){//获取最小节点
+        if (node.leftChild == null) return node;
+        return getMin(node.leftChild);
+    }
+    private Node deleteMin(Node node){//删除最小节点
+        if (node.leftChild == null) return node.rightChild;
+        node.leftChild = deleteMin(node.leftChild);
+        return node;
+    }
+
+
+    /**
+     * 2.1 BSTree的遍历之先序遍历
+     * 在之前先说一下先序遍历、中序遍历、后序遍历如何区分：：根左右、左根右、左右根，可以发现左右顺序一致，只是根的位置逐渐向后移动
+     */
+    public List preOrder(){
+        List<T> list = new ArrayList<>();
+        preOrder(list, root);
+        return list;
+    }
+    private void preOrder(List<T> list, Node cur){
+        if (cur != null) {
+            list.add((T)cur.data);
+            preOrder(list, cur.leftChild);
+            preOrder(list, cur.rightChild);
+        }
+    }
+
+    /**
+     * 2.2 BSTree的遍历之中序遍历
+     */
+    public List inOrder(){
+        List<T> list = new ArrayList<>();
+        inOrder(list, root);
+        return list;
+    }
+    private void inOrder(List<T> list, Node cur){
+        if (cur != null){
+            inOrder(list, cur.leftChild);
+            list.add((T) cur.data);
+            inOrder(list, cur.rightChild);
+        }
+    }
+
+    /**
+     * 2.3 BSTree的遍历之后序遍历
+     */
+    public List postOrder(){
+        List<T> list = new ArrayList<>();
+        postOrder(list, root);
+        return list;
+    }
+    private void postOrder(List<T> list, Node cur){
+        if (cur != null){
+            postOrder(list, cur.leftChild);
+            postOrder(list, cur.rightChild);
+            list.add((T) cur.data);
+        }
+    }
+
+    /**
+     * 2.4 BSTree的遍历之层序遍历
+     */
+    public List levelOrder(){
+        List<T> list = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null) queue.add(root);
+        Node tmp = null;
+        while (!queue.isEmpty()){
+            tmp = queue.poll();
+            list.add((T) tmp.data);
+            if (tmp.leftChild != null) queue.add(tmp.leftChild);
+            if (tmp.rightChild != null) queue.add(tmp.rightChild);
+        }
+        return list;
+    }
+
+
+    /**
+     * 3 二叉树是否对称？(注意哈：这个方法不是根据BSTree的，而是普通的二叉树！！！！)就当作剑指 offer上的一个算法题来看
+     * 思想：两个节点A和B对称等价于:
+     * 这两个节点上存储的值相等
+     * 节点A的左子树节点和节点B的右子树上的节点是对称的
+     * 节点A的右子树节点和节点A的左子树上的节点是对称的
+     *
+     * 上面是递归的思想，那么得递归的思想呢？
+     * 非递归的方式就是使用两个队列来实现，对左子树使用从左到右的层序遍历，对右子树使用从右到做的层序遍历，一层一层比较
+     */
+    public static boolean isSymmetrical(Node header){
+        if (header == null) return true;
+        return isSymmetrical(header.leftChild, header.rightChild);
+    }
+    private static boolean isSymmetrical(Node left, Node right){
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return (left.data.compareTo(right.data) == 0) && isSymmetrical(left.leftChild,right.rightChild) && isSymmetrical(left.rightChild, right.leftChild);
+    }
+
+    /**
+     * 4 二叉树的序列化与反序列化
+     * 思想：其实说白了就是用什么样的结构存储到磁盘，然后通过读磁盘就可以恢复原来二叉树的结构！
+     * 本方法规定：使用先序遍历的方式，把二叉树协程String，"/"表示一个节点值的结束，"#/"表示空节点
+     */
+    public static String preSerialization(Node header){//序列化
+        StringBuilder sb = new StringBuilder();
+        preSerialization(header, sb);
+        return sb.toString();
+    }
+    private static void preSerialization(Node header, StringBuilder sb){
+        if (header == null) {
+            sb.append("#/");
+        }else{
+            sb.append(header.data + "/");//典型的先序遍历方式
+            preSerialization(header.leftChild, sb);
+            preSerialization(header.rightChild, sb);
+        }
+    }
+
+    static int i = -1;
+    public static Node preDeserialization(String str){//反序列化
+        String[] strs = str.split("/");
+        return preDeserialization(strs);
+    }
+    private static Node preDeserialization(String[] strs){
+        Node node;
+        if (strs[++i].equals("#")){
+            node = null;
+        }else{
+            node = new Node(strs[i]);//同样是先序遍历的路子
+            node.leftChild = preDeserialization(strs);
+            node.rightChild = preDeserialization(strs);
+        }
+        return node;
+    }
+
+
+
 
     public static void main(String[] args) {
         BSTreeUtils<Integer> tree = new BSTreeUtils();
         tree.insert(1);
         tree.insert(5);
-        System.out.println(tree.root);
-        System.out.println(tree.find(1));
+        tree.insert(3);
+//        System.out.println(tree.root);
+//        tree.delete(5);
+//        System.out.println(tree.root);;
+//        System.out.println(tree.find(3));
+//        System.out.println(tree.root);
+//        System.out.println(tree.preOrder());
+//        System.out.println(tree.inOrder());
+//        System.out.println(tree.postOrder());
+//        System.out.println(tree.levelOrder());
+
+
+        //创建一个普通的二叉树
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node2 = new Node<>(2);
+        Node<Integer> node3 = new Node<>(2);
+        Node<Integer> node4 = new Node<>(3);
+        Node<Integer> node5 = new Node<>(3);
+        Node<Integer> node6 = new Node<>(4);
+        Node<Integer> node7 = new Node<>(4);
+        node1.leftChild = node2;
+        node1.rightChild = node3;
+        node2.leftChild = node4;
+        node2.rightChild = node6;
+        node3.leftChild = node7;
+        node3.rightChild = node5;
+        //System.out.println(isSymmetrical(node1));
+        //System.out.println(preSerialization(node1));
+        String str = "1/2/3/#/#/4/#/#/2/4/#/#/3/#/#/";
+        Node header = preDeserialization(str);
+        System.out.println(header);
+
     }
 }
